9f24b81a8c7b7fbd25d7958ec77e826df1eb8aed
Mikaela Mathisson <mikaela.mathisson@hotmail.se>
2023-09-21 16:03:59 +0200

    changes to js file

--- script.js
+++ script.js
@@ -18,22 +18,22 @@ function addTask(){
         let span = document.createElement("span");
         span.innerHTML = "\uD83D\uDDD1";
         li.appendChild(span);
-        arr.push(inputBox.value); //* Sparar all data som läggs till eller tas bort i listan lokalt.
+        arr.push(inputBox.value); //* Saves data to array
     }
 inputBox.value = "";
 }
 
 listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){                              //* Om jag klickar på uppgiften som skapats i listan, blir den ikryssad. Om redan ikryssad, så tas ikryssningen bort.
+    if(e.target.tagName === "LI"){                              // When click, it makes the task completed. If already completed (checked), the check gets removed.
         e.target.classList.toggle("checked");
         document.getElementById("count").innerHTML = count++;
     }
-    else if(e.target.tagName === "SPAN"){                      //* Vid klick på soptunnan så tas uppgiften bort från listan.
+    else if(e.target.tagName === "SPAN"){                      //* Click on trashcan deletes the task from the list
         e.target.parentElement.remove();  
         document.getElementById("count").innerHTML = count--;                                      
     }
     }, false);
 
-    console.log(arr);
+console.log(arr);
     
     
