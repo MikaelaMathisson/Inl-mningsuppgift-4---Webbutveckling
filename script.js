d7449dd8e20872039b7c0ea4e6971dfadda3cc75
Mikaela Mathisson <mikaela.mathisson@hotmail.se>
2023-09-23 16:55:16 +0200

    changes to files, commenting

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let arr = [];
 let count = 0;
 
@@ -5,23 +6,25 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 
+//Function to add task, for displaying alert message if inputbux is empty, creates trashcan icon, and also saving data to array
 function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!");
+    if(inputBox.value === ''){   
+        alert("You must write something!");     // If input box is empty, this alert message shows up
     }
     else{
-        let li = document.createElement("li");
+        let li = document.createElement("li"); // When something is written in the inputbox, this creates listitem and puts it in the list
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
-        span.innerHTML = "\uD83D\uDDD1";
+        span.innerHTML = "\uD83D\uDDD1";     // Trashcan icon
         li.appendChild(span);
-        arr.push(inputBox.value); //* Saves data to array
+        arr.push(inputBox.value); // Saves data to array
     }
 inputBox.value = "";
 }
 
-    listContainer.addEventListener("click", function(e){
+// Function to check if the task is completed or not, and then adds or removes from count
+    listContainer.addEventListener("click", function(e){    
         if (e.target.classList.contains('checked')) {
             e.target.classList.remove('checked')
             count--;
@@ -34,8 +37,10 @@ inputBox.value = "";
     
         });
 
+
+// Function for Click on trashcan deletes the task from the list 
 listContainer.addEventListener("click", function(e){
-        if(e.target.tagName === "SPAN"){                      // Click on trashcan deletes the task from the list
+        if(e.target.tagName === "SPAN"){                     
             e.target.parentElement.remove();
             count--;
             document.getElementById('count').innerHTML = count
